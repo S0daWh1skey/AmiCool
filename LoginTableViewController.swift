fdd3ad3641011202bd93dad45bd7dd0f768507bf
S0daWh1skey <albertsphepherd@alberts-macbook-pro.local>
2017-05-26 15:13:51 +0800

    Building The Login in Framework

---
+++ LoginTableViewController.swift
@@ -0,0 +1,44 @@
+//
+//  LoginTableViewController.swift
+//  ModalViewDemo
+//
+//  Created by Albert Sphepherd on 2017/5/26.
+//  Copyright © 2017年 Albert Sphepherd. All rights reserved.
+//
+
+import UIKit
+
+class LoginTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60.0
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
