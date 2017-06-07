427150bff9473822fea9919a1afdbcf1d57492f1
S0daWh1skey <albertsphepherd@alberts-macbook-pro.local>
2017-06-07 11:09:00 +0800

    完成视频界面

--- VideoController.swift
+++ VideoController.swift
@@ -16,7 +16,10 @@ class VideoController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 20/255.0, green: 155/255.0, blue: 1.0, alpha: 1.0)
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -34,9 +37,14 @@ class VideoController: UICollectionViewController {
             if videoList != nil{
                 DispatchQueue.main.async {
                     self.videoList = videoList
-                    
+                    self.collectionView?.reloadData()
                 }
             }
+            else{
+                    //提示，获取视频列表失败
+                
+            }
+            
         }
     }
 
@@ -70,10 +78,10 @@ class VideoController: UICollectionViewController {
         }
         return 1
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! VideoCell
-    
+        
         // Configure the cell
         if self.videoList != nil{
             let model = self.videoList[indexPath.row] as! VideoReturnJson
@@ -81,11 +89,16 @@ class VideoController: UICollectionViewController {
             cell.title.text = model.name
             cell.author.text = model.author
             
-            let imgUrlPre = "http://amicool.neusoft.edu.cn/Uploads/"
-            let imgUrl = URL(string: imgUrlPre + model.thumb!)
-            let imgData = try! Data(contentsOf: imgUrl!)
-            cell.img.image = UIImage(data: imgData)?.scaleImageToSize(CGSize(width: 80, height: 60))
+            if model.thumb != nil{
+                let imgUrlPre = "http://amicool.neusoft.edu.cn/Uploads/"
+                let imgUrl = URL(string: imgUrlPre + model.thumb!)
+                let imgData = try! Data(contentsOf: imgUrl!)
+                cell.img.image = UIImage(data: imgData)?.scaleImageToSize(CGSize(width: 80, height: 60))
             }
+            else{
+                cell.img.image = UIImage(named: "images/placeholder.png")
+            }
+        }
         
         
         return cell
