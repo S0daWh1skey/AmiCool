dd9a99a751bcbb6f06d8673495dd9c07bce66e45
S0daWh1skey <albertsphepherd@alberts-macbook-pro.local>
2017-06-12 22:38:55 +0800

    完成“我的”界面设计及搭建

--- MeController.swift
+++ MeController.swift
@@ -12,7 +12,7 @@ class MeController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.blue
+        //self.view.backgroundColor = UIColor.blue
         // Do any additional setup after loading the view.
     }
 
